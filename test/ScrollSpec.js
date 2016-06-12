d39affb0b1630c040041567c08ca5470e17669b2
nick <ad60@nicks-MacBook-Pro.local>
2016-06-12 18:56:25 -0400

    use renderer electron-mocha

--- test/ScrollSpec.js
+++ test/ScrollSpec.js
@@ -2,8 +2,14 @@ import assert from 'assert';
 import Scroll from '../src/index';
 
 describe('Scroll', () => {
+
+  it('uses electron test renderer', () => {
+    assert.equal(typeof document, 'object');
+  });
+
   it('is a valid class', () => {
     const instance = new Scroll();
     assert.equal(instance.scrollPos.length, 0);
   });
+
 });
