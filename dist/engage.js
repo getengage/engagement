252312ee671032dec1ed2e1909121af93a8c68b7
Nick Soto <nickesoto@gmail.com>
2017-01-01 03:15:23 -0500

    polyfill object.assign

--- dist/engage.js
+++ dist/engage.js
@@ -354,6 +354,8 @@ var engage =
 
 	"use strict";
 	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
 	var $$ = function () {
@@ -362,7 +364,7 @@ var engage =
 	  }
 	
 	  $$.extend = function extend(obj1, obj2) {
-	    return Object.assign(Object.create(Object.prototype), obj1, obj2);
+	    return _extends(Object.create(Object.prototype), obj1, obj2);
 	  };
 	
 	  return $$;
